d682718ec5922af59570e873aad051c34e671411
Emilio <emiliosneto13@gmail.com>
2023-03-12 04:00:12 -0300

    Implement login form and request (no layout)

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,37 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { api } from "../../services/api";
+
+const loginFormSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
+type LoginFormData = z.infer<typeof loginFormSchema>;
+
 export function LoginPage() {
-  return <h1>Login</h1>;
+  const { register, handleSubmit } = useForm<LoginFormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  async function handleLogin(data: LoginFormData) {
+    const res = await api.post("/auth/login", {
+      email: data.email,
+      password: data.password,
+    });
+
+    console.log(res);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(handleLogin)}>
+      <label>Email</label>
+      <input {...register("email")} />
+      <label>Senha</label>
+      <input {...register("password")} type="password" />
+      <button type="submit">LOGAR</button>
+      <button>CRIAR CONTA</button>
+    </form>
+  );
 }
