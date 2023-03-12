5e01e31479f461ea4734ec0c791cf73a01502e16
Emilio <emiliosneto13@gmail.com>
2023-03-12 04:35:35 -0300

    Use token and remove token from redux using axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
+import { store } from "../store";
+import { logout } from "../store/modules/auth/actions";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
 api.interceptors.request.use((config) => {
-  // const token = ...
-  // if (token) config.headers["Authorization"] = `Bearer ${token}`;
+  const token = store.getState().auth.token;
+
+  if (token) config.headers["Authorization"] = `Bearer ${token}`;
 
   return config;
 });
@@ -15,7 +18,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      // todo remove token from redux if exists
+      store.dispatch(logout());
+      return error.response;
     }
 
     return Promise.reject(error);
