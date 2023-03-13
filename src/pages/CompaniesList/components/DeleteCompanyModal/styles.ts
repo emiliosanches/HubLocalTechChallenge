b33c615557cfbeb6a53480cca242ea371ec5e57c
Emilio <emiliosneto13@gmail.com>
2023-03-13 18:44:21 -0300

    Remove unused CSS from delete modals

--- src/pages/CompaniesList/components/DeleteCompanyModal/styles.ts
+++ src/pages/CompaniesList/components/DeleteCompanyModal/styles.ts
@@ -25,40 +25,6 @@ export const StyledDialog = styled(Dialog)`
       color: white;
     }
   }
-
-  & .MuiDialogContent-root {
-    display: flex;
-    justify-content: center;
-  }
-
-  #company-form-loading {
-    align-self: center;
-  }
-`;
-
-export const CompanyForm = styled.form`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  column-gap: 2.25rem;
-  height: fit-content;
-
-  .InputContainer {
-    margin-bottom: 0.5rem;
-  }
-
-  .fill-line {
-    width: 100%;
-  }
-
-  & > div:not(.fill-line) {
-    width: calc(50% - 1.125rem);
-  }
-
-  input {
-    height: 2.75rem;
-    font-size: 1.125rem;
-  }
 `;
 
 export const DeleteButton = styled.button`
